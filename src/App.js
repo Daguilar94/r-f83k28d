df9c56358babd5fcdb0a0358d4163ab3f1029fdb
David Aguilar <davidaguilarh.94@gmail.com>
2018-01-08 02:02:17 -0500

    Fix increment by 1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      val: 0
+    }
+  }
   render() {
+
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.val}</span>
+        <button id="inc" onClick={this.increment.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+
+  increment() {
+    this.setState({
+      val : this.state.val + 1
+    })
   }
 }
 
